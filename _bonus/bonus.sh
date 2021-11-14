7a55b0b9c3a3b523f56f7861aa005b4745ac0bb3
rafa <rafa>
2021-11-14 23:54:27 +0100

    xd

--- _bonus/bonus.sh
+++ _bonus/bonus.sh
@@ -1,2 +1,2 @@
-gcc -o bonus_client bonus_client.c itoa.c atoi.c
-gcc -o bonus_server bonus_server.c itoa.c atoi.c bonus_confirm.c
+gcc -o bonus_client bonus_client.c ../itoa.c ../atoi.c
+gcc -o bonus_server bonus_server.c ../itoa.c ../atoi.c bonus_confirm.c
